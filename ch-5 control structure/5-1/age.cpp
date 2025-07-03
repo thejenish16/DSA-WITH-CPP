80abc4bc2cc9d9704c0aa0b4304355705166911d
thejenish16 <jenishpardava16@gmail.com>
2025-07-03 11:03:36 +0530

    ch-5

---
+++ ch-5 control structure/5-1/age.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    int age;
+    cout << "Entar Your Age";
+    cin >> age;
+
+    if (age > 18)
+    {
+        cout << "Entar Can Vote";
+    }
+    else
+    {
+        cout << " You Can Not Vote";
+    }
+}
